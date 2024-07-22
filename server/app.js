0302447cb6d28bbad65cff06a4c954401b6db080
Demilade Julius <demilade3chub@gmail.com>
2024-07-22 15:10:54 +0100

    updated the port on user

--- server/app.js
+++ server/app.js
@@ -2,22 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
 const path = require('path');
-const app = express();
 const cors = require('cors');  // Import the cors middleware
-const port = process.env.PORT || 4900;
-
+const app = express();
+const port = process.env.PORT || 4700;
+const dotenv = require('dotenv');
+dotenv.config()
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect('mongodb+srv://shorten:142580@cluster0.gpbwmkj.mongodb.net/urlShortener')
-.then(()=>{
-  console.log('database connection established')
-
+.then(() => {
+  console.log('database connection established');
 })
 .catch(err => {
-  console.log(err)
+  console.log(err);
 });
 
 const urlSchema = new mongoose.Schema({
@@ -40,22 +40,28 @@ app.post('/shorten', async (req, res) => {
 
   await newURL.save();
 
-  res.json({ shortURL:`https://shortlink-puce.vercel.app/${shortCode}` });
-});
+  const baseURL = process.env.BASE_URL;
+  const shortURL = `${baseURL}/${shortCode}`;
+
+  res.json({ shortURL });});
 
 app.get('/:shortCode', async (req, res) => {
   console.log('Received GET request to /:shortCode');
 
   const shortCode = req.params.shortCode;
+  console.log('Searching for shortCode:', shortCode);
+
   const urlData = await UrlModel.findOne({ shortCode });
 
   if (urlData) {
+    console.log('URL found:', urlData.longURL);
     res.redirect(urlData.longURL);
   } else {
+    console.log('URL not found for shortCode:', shortCode);
     res.status(404).json({ error: 'URL not found' });
   }
-})
+});
 
-app.listen(port , () => {
-    console.log(`listening to port ${port}`)
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
 });
